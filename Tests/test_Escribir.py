a9e7e54a24872dcdcd48cd72429b58a3fe83a227
LucBell <lucrecialabella@hotmail.com>
2022-11-19 12:48:37 +0000

    Tests para escribir en ficheros

--- Tests/test_Escribir.py
+++ Tests/test_Escribir.py
@@ -1,5 +1,11 @@
-with open('test.txt', 'w') as f:
+# First create a file, with a couple of lines
+with open('Tests/test.txt', 'w') as f:
     for i in range(1, 5):
-        f.write(str(i))
-with open('test.txt', 'r') as f:
+        f.write(f'Number {i}\n')
+
+# Now add some extra lines using append mode
+with open('Tests/test.txt', 'a') as f:
+    for i in range(5, 20):
+        f.write(f'Append number {i}\n')
+with open('Tests/test.txt') as f:
     print(f.read())
